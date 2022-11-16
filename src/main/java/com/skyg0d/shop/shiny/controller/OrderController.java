fa7a3229b23d389b28dc230416a982db651044bd
SkyG0D <silvanosilvino@hotmail.com>
2022-11-16 19:43:39 -0300

    docs: add order docs

--- src/main/java/com/skyg0d/shop/shiny/controller/OrderController.java
+++ src/main/java/com/skyg0d/shop/shiny/controller/OrderController.java
@@ -7,6 +7,9 @@ import com.skyg0d.shop.shiny.payload.response.MessageResponse;
 import com.skyg0d.shop.shiny.payload.response.OrderResponse;
 import com.skyg0d.shop.shiny.security.service.UserDetailsImpl;
 import com.skyg0d.shop.shiny.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -27,11 +30,25 @@ public class OrderController {
 
     @GetMapping
     @IsAdmin
+    @Operation(summary = "Returns all orders with pagination", tags = "Orders")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successful"),
+            @ApiResponse(responseCode = "401", description = "When not authorized"),
+            @ApiResponse(responseCode = "403", description = "When forbidden"),
+            @ApiResponse(responseCode = "500", description = "When server error")
+    })
     public ResponseEntity<Page<OrderResponse>> listAll(Pageable pageable) {
         return ResponseEntity.ok(orderService.listAll(pageable));
     }
 
     @GetMapping("/my")
+    @Operation(summary = "Returns all orders of an user with pagination", tags = "Orders")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successful"),
+            @ApiResponse(responseCode = "401", description = "When not authorized"),
+            @ApiResponse(responseCode = "403", description = "When forbidden"),
+            @ApiResponse(responseCode = "500", description = "When server error")
+    })
     public ResponseEntity<Page<OrderResponse>> listAllByUser(Pageable pageable) {
         UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder
                 .getContext()
@@ -42,16 +59,32 @@ public class OrderController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Returns order by id", tags = "Orders")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successful"),
+            @ApiResponse(responseCode = "400", description = "When category not found"),
+            @ApiResponse(responseCode = "500", description = "When server error")
+    })
     public ResponseEntity<OrderResponse> findById(@PathVariable String id) {
         return ResponseEntity.ok(orderService.findByIdMapped(id));
     }
 
     @PostMapping
+    @Operation(summary = "Persists a new order", tags = "Orders")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successful"),
+            @ApiResponse(responseCode = "500", description = "When server error")
+    })
     public ResponseEntity<OrderResponse> create(@RequestBody @Valid CreateOrderRequest request) {
         return new ResponseEntity<>(orderService.create(request), HttpStatus.CREATED);
     }
 
     @PatchMapping("/{id}/cancel")
+    @Operation(summary = "Updates order status to canceled", tags = "Orders")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successful"),
+            @ApiResponse(responseCode = "500", description = "When server error")
+    })
     public ResponseEntity<MessageResponse> cancelOrder(@PathVariable String id) {
         orderService.cancelOrder(id);
 
@@ -60,6 +93,13 @@ public class OrderController {
 
     @PatchMapping("/{id}/ship")
     @IsAdmin
+    @Operation(summary = "Updates order status to shipped", tags = "Orders")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successful"),
+            @ApiResponse(responseCode = "401", description = "When not authorized"),
+            @ApiResponse(responseCode = "403", description = "When forbidden"),
+            @ApiResponse(responseCode = "500", description = "When server error")
+    })
     public ResponseEntity<MessageResponse> shipOrder(@PathVariable String id) {
         orderService.adminChangeStatus(id, EOrderStatus.SHIPPED, "Order canceled, could not ship.");
 
@@ -68,6 +108,13 @@ public class OrderController {
 
     @PatchMapping("/{id}/otw")
     @IsAdmin
+    @Operation(summary = "Updates order status to on the way", tags = "Orders")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successful"),
+            @ApiResponse(responseCode = "401", description = "When not authorized"),
+            @ApiResponse(responseCode = "403", description = "When forbidden"),
+            @ApiResponse(responseCode = "500", description = "When server error")
+    })
     public ResponseEntity<MessageResponse> onTheWayOrder(@PathVariable String id) {
         orderService.adminChangeStatus(id, EOrderStatus.ON_THE_WAY, "Order canceled, could not ship.");
 
@@ -76,6 +123,13 @@ public class OrderController {
 
     @PatchMapping("/{id}/deliver")
     @IsAdmin
+    @Operation(summary = "Updates order status to delivered", tags = "Orders")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successful"),
+            @ApiResponse(responseCode = "401", description = "When not authorized"),
+            @ApiResponse(responseCode = "403", description = "When forbidden"),
+            @ApiResponse(responseCode = "500", description = "When server error")
+    })
     public ResponseEntity<MessageResponse> deliverOrder(@PathVariable String id) {
         orderService.adminChangeStatus(id, EOrderStatus.DELIVERED, "Order canceled, could not deliver.");
 
