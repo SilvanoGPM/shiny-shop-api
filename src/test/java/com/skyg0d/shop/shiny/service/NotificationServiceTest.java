286c7371ceae9467eaed26919054862ada2ba99c
SkyG0D <silvanosilvino@hotmail.com>
2022-12-29 22:11:26 -0300

    test: add ALL tests to NotificationService

--- src/test/java/com/skyg0d/shop/shiny/service/NotificationServiceTest.java
+++ src/test/java/com/skyg0d/shop/shiny/service/NotificationServiceTest.java
@@ -1,7 +1,9 @@
 package com.skyg0d.shop.shiny.service;
 
+import com.skyg0d.shop.shiny.exception.PermissionInsufficient;
 import com.skyg0d.shop.shiny.model.Notification;
 import com.skyg0d.shop.shiny.model.User;
+import com.skyg0d.shop.shiny.payload.request.CreateNotificationToAllRequest;
 import com.skyg0d.shop.shiny.payload.response.CountNotificationsResponse;
 import com.skyg0d.shop.shiny.payload.response.NotificationResponse;
 import com.skyg0d.shop.shiny.repository.NotificationRepository;
@@ -18,12 +20,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-import static com.skyg0d.shop.shiny.util.notification.NotificationCreator.createNotification;
+import static com.skyg0d.shop.shiny.util.notification.NotificationCreator.*;
 import static com.skyg0d.shop.shiny.util.user.UserCreator.createUser;
 import static org.assertj.core.api.Assertions.*;
 
@@ -65,8 +70,32 @@ public class NotificationServiceTest {
                 .thenReturn(readNotificationsPage);
 
         BDDMockito
+                .when(notificationRepository.findAll(ArgumentMatchers.<Specification<Notification>>any(), ArgumentMatchers.any(Pageable.class)))
+                .thenReturn(unreadNotificationsPage);
+
+        BDDMockito
                 .when(notificationRepository.countAllByUserAndCanceledAtIsNullAndReadAtIsNull(ArgumentMatchers.any(User.class)))
                 .thenReturn(1);
+
+        BDDMockito
+                .when(userService.findByEmail(ArgumentMatchers.anyString()))
+                .thenReturn(createUser());
+
+        BDDMockito
+                .when(userService.listAll())
+                .thenReturn(List.of(createUser()));
+
+        BDDMockito
+                .when(notificationRepository.save(ArgumentMatchers.any(Notification.class)))
+                .thenReturn(createNotification());
+
+        BDDMockito
+                .when(authUtils.isOwnerOrStaff(ArgumentMatchers.anyString()))
+                .thenReturn(true);
+
+        BDDMockito
+                .when(notificationRepository.findById(ArgumentMatchers.any(UUID.class)))
+                .thenReturn(Optional.of(createNotification()));
     }
 
     @Test
@@ -113,6 +142,84 @@ public class NotificationServiceTest {
         assertThat(count).isNotNull();
 
         assertThat(count.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("search Returns List Of Notifications Inside Page Object When Successful")
+    void search_ReturnsListOfNotificationsInsidePageObject_WhenSuccessful() {
+        Notification expectedNotification = createNotification();
+
+        Page<NotificationResponse> notificationsPage = notificationService.search(PageRequest.of(0, 1), createNotificationParameterSearch());
+
+        assertThat(notificationsPage).isNotNull();
+
+        assertThat(notificationsPage.getContent()).isNotEmpty();
+
+        assertThat(notificationsPage.getContent().get(0)).isNotNull();
+
+        assertThat(notificationsPage.getContent().get(0).getContent()).isEqualTo(expectedNotification.getContent());
+    }
+
+    @Test
+    @DisplayName("create Persists Notification When Successful")
+    void create_PersistsNotification_WhenSuccessful() {
+        Notification expectedNotification = createNotification();
+
+        NotificationResponse createdNotification = notificationService.create(createCreateNotificationRequest());
+
+        assertThat(createdNotification).isNotNull();
+
+        assertThat(createdNotification.getContent()).isEqualTo(expectedNotification.getContent());
+    }
+
+    @Test
+    @DisplayName("createToAllUsers Persists Notification To All Users When Successful")
+    void createToAllUsers_PersistsNotificationToAllUsers_WhenSuccessful() {
+        assertThatCode(() -> notificationService.createToAllUsers(createCreateNotificationToAllRequest()))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("read Updates Read At Notification To Now When Successful")
+    void read_UpdatesReadAtNotificationToNow_WhenSuccessful() {
+        assertThatCode(() -> notificationService.read(UUID.randomUUID().toString()))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("read Throws PermissionInsufficient When User Has No Permission")
+    void read_ThrowsPermissionInsufficient_WhenUserHasNoPermission() {
+        BDDMockito
+                .when(authUtils.isOwnerOrStaff(ArgumentMatchers.anyString()))
+                .thenReturn(false);
+
+        assertThatExceptionOfType(PermissionInsufficient.class)
+                .isThrownBy(() -> notificationService.read(UUID.randomUUID().toString()));
+    }
+
+    @Test
+    @DisplayName("unread Updates Read At Notification To Null When Successful")
+    void unread_UpdatesReadAtNotificationToNull_WhenSuccessful() {
+        assertThatCode(() -> notificationService.unread(UUID.randomUUID().toString()))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("unread Throws PermissionInsufficient When User Has No Permission")
+    void unread_ThrowsPermissionInsufficient_WhenUserHasNoPermission() {
+        BDDMockito
+                .when(authUtils.isOwnerOrStaff(ArgumentMatchers.anyString()))
+                .thenReturn(false);
+
+        assertThatExceptionOfType(PermissionInsufficient.class)
+                .isThrownBy(() -> notificationService.unread(UUID.randomUUID().toString()));
+    }
+
+    @Test
+    @DisplayName("cancel Updates Canceled At Notification To Now When Successful")
+    void cancel_UpdatesCanceledAtNotificationToNow_WhenSuccessful() {
+        assertThatCode(() -> notificationService.cancel(UUID.randomUUID().toString()))
+                .doesNotThrowAnyException();
     }
 
 }
--- src/test/java/com/skyg0d/shop/shiny/util/notification/NotificationCreator.java
+++ src/test/java/com/skyg0d/shop/shiny/util/notification/NotificationCreator.java
@@ -2,7 +2,10 @@ package com.skyg0d.shop.shiny.util.notification;
 
 import com.skyg0d.shop.shiny.mapper.NotificationMapper;
 import com.skyg0d.shop.shiny.model.Notification;
+import com.skyg0d.shop.shiny.payload.request.CreateNotificationRequest;
+import com.skyg0d.shop.shiny.payload.request.CreateNotificationToAllRequest;
 import com.skyg0d.shop.shiny.payload.response.NotificationResponse;
+import com.skyg0d.shop.shiny.payload.search.NotificationParameterSearch;
 
 import static com.skyg0d.shop.shiny.util.user.UserCreator.createUser;
 
@@ -22,6 +25,30 @@ public class NotificationCreator {
 
     public static NotificationResponse createNotificationResponse() {
         return NotificationMapper.INSTANCE.toNotificationResponse(createNotification());
+    }
+
+    public static NotificationParameterSearch createNotificationParameterSearch() {
+        return NotificationParameterSearch
+                .builder()
+                .content(CONTENT)
+                .build();
+    }
+
+    public static CreateNotificationRequest createCreateNotificationRequest() {
+        return CreateNotificationRequest
+                .builder()
+                .userEmail(createUser().getEmail())
+                .category(CATEGORY)
+                .content(CONTENT)
+                .build();
+    }
+
+    public static CreateNotificationToAllRequest createCreateNotificationToAllRequest() {
+        return CreateNotificationToAllRequest
+                .builder()
+                .category(CATEGORY)
+                .content(CONTENT)
+                .build();
     }
 
 }
